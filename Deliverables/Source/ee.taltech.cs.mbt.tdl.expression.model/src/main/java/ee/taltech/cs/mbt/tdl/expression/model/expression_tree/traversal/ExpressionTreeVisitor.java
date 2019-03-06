f4bfd6d25b0cf155cfe91f9ca5e62c65110347c6
Tanel Prikk <tanel.prikk@outlook.com>
2019-03-06 23:13:42 +0200

    Remove empty abstract class (#25).

--- Deliverables/Source/ee.taltech.cs.mbt.tdl.expression.model/src/main/java/ee/taltech/cs/mbt/tdl/expression/model/expression_tree/traversal/ExpressionTreeVisitor.java
+++
@@ -1,9 +0,0 @@
-package ee.taltech.cs.mbt.tdl.expression.model.expression_tree.traversal;
-
-import ee.taltech.cs.mbt.tdl.expression.model.expression_tree.structure.concrete.ExpressionTree;
-import ee.taltech.cs.mbt.tdl.expression.model.expression_tree.structure.generic.node.internal.logical.AbsLogicalOperatorNode;
-
-public abstract class ExpressionTreeVisitor
-	extends AbsExpressionTreeVisitor<ExpressionTree, AbsLogicalOperatorNode<?>>
-{
-}
