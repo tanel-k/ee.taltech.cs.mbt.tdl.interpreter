e186d366b410df3a054965e6cf19fe70f24e7e24
Tanel Prikk <tanel.prikk@outlook.com>
2019-03-11 01:28:48 +0200

    Unnecessary generic arguments for ExpressionTree (#26).

--- Deliverables/Source/ee.taltech.cs.mbt.tdl.expression.model/src/main/java/ee/taltech/cs/mbt/tdl/expression/model/expression_tree/structure/concrete/ExpressionTree.java
+++ Deliverables/Source/ee.taltech.cs.mbt.tdl.expression.model/src/main/java/ee/taltech/cs/mbt/tdl/expression/model/expression_tree/structure/concrete/ExpressionTree.java
@@ -3,6 +3,6 @@ package ee.taltech.cs.mbt.tdl.expression.model.expression_tree.structure.concret
 import ee.taltech.cs.mbt.tdl.expression.model.expression_tree.structure.concrete.internal.logical.generic.AbsLogicalOperatorNode;
 import ee.taltech.cs.mbt.tdl.expression.model.expression_tree.structure.generic.AbsExpressionTree;
 
-public class ExpressionTree<RootType extends AbsLogicalOperatorNode> extends AbsExpressionTree<RootType> {
+public class ExpressionTree extends AbsExpressionTree<AbsLogicalOperatorNode> {
 	public ExpressionTree() { }
 }
