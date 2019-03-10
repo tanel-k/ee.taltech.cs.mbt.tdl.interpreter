8d956e2b7629852c13993de8064d390793671bd4
Tanel Prikk <tanel.prikk@outlook.com>
2019-03-11 01:29:51 +0200

    Parser stub (#26).

--- Deliverables/Source/ee.taltech.cs.mbt.tdl.expression.parser/src/main/java/ee/taltech/cs/mbt/tdl/expression/parser/ExpressionParser.java
+++ Deliverables/Source/ee.taltech.cs.mbt.tdl.expression.parser/src/main/java/ee/taltech/cs/mbt/tdl/expression/parser/ExpressionParser.java
@@ -1,23 +1,43 @@
 package ee.taltech.cs.mbt.tdl.expression.parser;
 
+import ee.taltech.cs.mbt.tdl.expression.grammar_facade.parsing.ParserFacade;
 import ee.taltech.cs.mbt.tdl.expression.model.expression_tree.structure.concrete.ExpressionTree;
 
+import javax.swing.text.html.parser.Parser;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class ExpressionParser {
-	// FIXME: Adjust according to changes in GrammarFacade
-//	public ExpressionTree parseExpression(String expression, Charset charset) {
-//		try {
-//			return parseExpression(new ByteArrayInputStream(expression.getBytes(charset)));
-//		} catch (IOException ex) {
-//			throw new RuntimeException(ex);
-//		}
-//	}
-//
-//	public ExpressionTree parseExpression(InputStream in) throws IOException {
-//		return new GrammarFacade().parse(in);
-//	}
+	private ParserFacade parserFacade;
+
+	public ExpressionParser() {
+		this(new ParserFacade());
+	}
+
+	public ExpressionParser(ParserFacade parserFacade) {
+		this.parserFacade = parserFacade;
+	}
+
+	public ParserFacade getParserFacade() {
+		return parserFacade;
+	}
+
+	public ExpressionTree parseExpression(String expression) {
+		return parseExpression(expression, StandardCharsets.UTF_8);
+	}
+
+	public ExpressionTree parseExpression(String expression, Charset charset) {
+		try {
+			return parseExpression(new ByteArrayInputStream(expression.getBytes(charset)));
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	public ExpressionTree parseExpression(InputStream in) throws IOException {
+		return parserFacade.parse(in);
+	}
 }
---
+++ Deliverables/Source/ee.taltech.cs.mbt.tdl.expression.parser/src/main/java/ee/taltech/cs/mbt/tdl/expression/parser/Test.java
@@ -0,0 +1,15 @@
+package ee.taltech.cs.mbt.tdl.expression.parser;
+
+import ee.taltech.cs.mbt.tdl.expression.model.expression_tree.structure.concrete.ExpressionTree;
+
+import java.nio.charset.StandardCharsets;
+
+public class Test {
+	public static void main(String... args) {
+		String input = "U(!TR1) -> E(TR1;TR2)";
+		ExpressionParser expressionParser = new ExpressionParser();
+		expressionParser.getParserFacade().getErrorStrategyConfig().setRecoveryEnabled(false);
+		ExpressionTree tree = expressionParser.parseExpression(input);
+		tree.getRootNode();
+	}
+}
