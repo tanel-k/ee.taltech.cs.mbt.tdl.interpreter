7ce21707ff2813e24ea5921dc7da10c882b54bda
Tanel Prikk <tanel.prikk@outlook.com>
2019-03-16 13:46:00 +0200

    Restore deleted files (#27, #28).

---
+++ Deliverables/Source/ee.taltech.cs.mbt.tdl.uppaal_system.uppaal_system_model/src/main/java/ee/taltech/cs/mbt/tdl/uppaal_system/uppaal_system_model/uta_structural_model/transitions/UTATransitionLabelContainer.java
@@ -0,0 +1,44 @@
+package ee.taltech.cs.mbt.tdl.uppaal_system.uppaal_system_model.uta_structural_model.transitions;
+
+import ee.taltech.cs.mbt.tdl.uppaal_system.uppaal_system_model.uta_structural_model.labels.*;
+
+public class UTATransitionLabelContainer {
+	private UTAGuardLabel guardLabel;
+	private UTAAssignmentsLabel assignmentsLabel;
+	private UTASelectLabel selectLabel;
+	private UTASynchronizationLabel synchronizationLabel;
+
+	private UTACommentLabel commentLabel;
+
+	public UTAGuardLabel getGuardLabel() {
+		return guardLabel;
+	}
+
+	public void setGuardLabel(UTAGuardLabel guardLabel) {
+		this.guardLabel = guardLabel;
+	}
+
+	public UTAAssignmentsLabel getAssignmentsLabel() {
+		return assignmentsLabel;
+	}
+
+	public void setAssignmentsLabel(UTAAssignmentsLabel assignmentsLabel) {
+		this.assignmentsLabel = assignmentsLabel;
+	}
+
+	public UTASelectLabel getSelectLabel() {
+		return selectLabel;
+	}
+
+	public void setSelectLabel(UTASelectLabel selectLabel) {
+		this.selectLabel = selectLabel;
+	}
+
+	public UTASynchronizationLabel getSynchronizationLabel() {
+		return synchronizationLabel;
+	}
+
+	public void setSynchronizationLabel(UTASynchronizationLabel synchronizationLabel) {
+		this.synchronizationLabel = synchronizationLabel;
+	}
+}
