422879f1707d9b2263dc02b9b2c12de629c82c98
Tanel Prikk <tanel.prikk@outlook.com>
2019-03-21 17:07:57 +0200

    Neglected to add XmlNodeNta.java prior to commit (#27, #28).

---
+++ Deliverables/Source/ee.taltech.cs.mbt.tdl.uppaal.uta_parser/src/main/java/ee/taltech/cs/mbt/tdl/uppaal/tdl_parser/xml/jaxb/XmlNodeNta.java
@@ -0,0 +1,31 @@
+
+package ee.taltech.cs.mbt.tdl.uppaal.tdl_parser.xml.jaxb;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * An UPPAAL timed-automata system description.
+ * 
+ * <p>Java class for nta element declaration.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;element name="nta" type="{}NtaSystemType"/&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+@XmlRootElement(name = "nta")
+public class XmlNodeNta
+    extends XmlNodeNtaSystem
+{
+
+
+}
