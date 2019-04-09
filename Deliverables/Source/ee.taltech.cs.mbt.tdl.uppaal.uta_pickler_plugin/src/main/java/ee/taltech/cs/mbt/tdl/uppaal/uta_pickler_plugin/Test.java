cddcfb33288ce77f4a064689bcad7db30129e20e
Tanel Prikk <tanel.prikk@outlook.com>
2019-04-09 23:54:45 +0300

    Fix basic syntax errors in pickler template + new TODO (#29, #31).

---
+++ Deliverables/Source/ee.taltech.cs.mbt.tdl.uppaal.uta_pickler_plugin/src/main/java/ee/taltech/cs/mbt/tdl/uppaal/uta_pickler_plugin/Test.java
@@ -0,0 +1,9 @@
+package ee.taltech.cs.mbt.tdl.uppaal.uta_pickler_plugin;
+
+import ee.taltech.cs.mbt.tdl.uppaal.uta_pickler_plugin.pickle_generator.SystemPickleGeneratorFactory;
+
+public class Test {
+	public static void main(String... args) {
+		SystemPickleGeneratorFactory.systemGenerator("test", "test");
+	}
+}
