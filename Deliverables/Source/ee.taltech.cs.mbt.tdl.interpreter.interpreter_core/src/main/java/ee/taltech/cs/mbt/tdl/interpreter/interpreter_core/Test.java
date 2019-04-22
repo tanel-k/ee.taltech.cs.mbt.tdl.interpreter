2145ba83c624e2b4f7937d5049dbaf220b06da78
LAPTOP-2QDSBUU6\tanel <tanel.prikk@outlook.com>
2019-04-22 12:24:00 +0300

    Successfully installed on second machine; synchronizing.

--- Deliverables/Source/ee.taltech.cs.mbt.tdl.interpreter.interpreter_core/src/main/java/ee/taltech/cs/mbt/tdl/interpreter/interpreter_core/Test.java
+++ Deliverables/Source/ee.taltech.cs.mbt.tdl.interpreter.interpreter_core/src/main/java/ee/taltech/cs/mbt/tdl/interpreter/interpreter_core/Test.java
@@ -4,10 +4,12 @@ import java.io.ByteArrayInputStream;
 
 public class Test {
 	public static void main(String... args) {
+		// A(TS2;TS4) ~> E(TS2;TS3): U(TS2;TS4) ~> E(TS2;TS3)
+		// #(E(TS2;TS4) ~> A(TS2;TS3)) >= 2: #[>=2](E(TS2;TS4) ~> U(TS2;TS3))
 		Interpreter.getInstance().interpret(
 				Test.class.getResourceAsStream("/SampleSystem.xml"),
 				new ByteArrayInputStream(
-						"E(TS1; TS2)".getBytes()
+						"#[>=2](E(TS2;TS4) ~> U(TS2;TS3))".getBytes()
 				),
 				System.out
 		);
